74eb05429307c4a843fe6421a4fbb7a554935f09
Gary Saunders <gary@codenamegary.com>
2014-06-24 23:06:50 -0400

    Composer config and repository interface.

---
+++ src/Repository.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace codenamegary\L4Utils;
+
+use \Illuminate\Database\Eloquent\Model;
+
+interface Repository {
+    
+    public function get($id, array $related = array());
+    
+    public function update($id, array $data = array());
+    
+    public function create(array $data);
+    
+    public function delete($id);
+    
+    public function make(array $data = array());
+    
+    public function save(Model $model);
+    
+}
